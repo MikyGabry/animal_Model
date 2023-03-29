8713bf80f81e26dafcea27cc449586c180be79b9
Miky.Gabry <isgromichael@gmail.com>
2023-03-28 23:28:12 -0400

    Iniziato a creare i file .ejs 'header'

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 
+app.get('/', (req, res) => {
+    res.send('hello word')
+})
 
 app.listen(PORT, () => {
     console.log(`🐳🦋 Server is listening to PORT ${PORT} 🐢🦅`)
