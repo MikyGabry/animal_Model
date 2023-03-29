01a7d59379342f7a4013c6e64b9c58dc6fe19881
Miky.Gabry <isgromichael@gmail.com>
2023-03-29 10:16:53 -0400

    Creata la pagina index e collegata con server

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const PORT = 4000;
+const animalsController = require('./controllers/animals')
+
+app.set('view engine', 'ejs');
+// app.use(express.urlencoded({ extended:false }));
+
 
 app.get('/', (req, res) => {
-    res.send('hello word')
+    res.render('home.ejs')
 })
+
+app.use('/animals', animalsController)
 
 app.listen(PORT, () => {
     console.log(`🐳🦋 Server is listening to PORT ${PORT} 🐢🦅`)
